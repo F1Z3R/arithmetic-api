c54199bcd79c1d6e5bbc3a30c9dd68f01329c48b
F1Z3R <elhassany.alae@etu.uae.ac.ma>
2025-10-15 00:01:38 +0100

    Update watcher.sh

--- watcher.sh
+++ watcher.sh
@@ -5,12 +5,10 @@ PORT=5000
 
 echo "[INFO] Starting watcher for $APP_NAME..."
 
-# Function to build and deploy
 build_and_run() {
     echo "[INFO] Change detected. Rebuilding Docker image..."
     docker build -t $IMAGE_NAME .
     
-    # Stop and remove existing container if running
     if [ "$(docker ps -q -f name=$APP_NAME)" ]; then
         docker stop $APP_NAME && docker rm $APP_NAME
     fi
@@ -19,10 +17,8 @@ build_and_run() {
     docker run -d -p $PORT:5000 --name $APP_NAME $IMAGE_NAME
 }
 
-# Initial build
 build_and_run
 
-# Monitor for changes
 inotifywait -m -e modify,create,delete ./ |
 while read path action file; do
     echo "[INFO] Detected change in $file"
